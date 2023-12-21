404e76de71799bb5d11a532181fab68288667876
erosalpe <eros.alpegiani@gmail.com>
2023-12-21 13:03:42 +0100

    feat(js) aggiunti prompt per i dati

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+let nome = prompt("Inserisci nome?");
+let cognome = prompt("Inserisci cognome");
+let colorePreferito = prompt("Inserisci colore preferito");
+
+document.getElementById("dati").innerHTML = `${nome} ${cognome} ${colorePreferito} 23`;
